b7247be24f015824a42e7fb961f4b319636f339f
Kanak Yadav <yadavkanak109@gmail.com>
2026-08-10 10:22:49 +0530

    feat: implement user authentication controller and Nodemailer service for email notifications

--- backend/src/service/mail.service.js
+++ backend/src/service/mail.service.js
@@ -2,14 +2,19 @@ require("dotenv").config();
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-   service  : "gmail",
-   auth: {
-        type: "OAuth2",
-        user: process.env.EMAIL_USER,
-        clientId: process.env.CLIENT_ID,
-        clientSecret: process.env.CLIENT_SECRET,
-        refreshToken: process.env.REFRESH_TOKEN,
-      },
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true, // true for port 465, false for port 587
+  auth: {
+    type: "OAuth2",
+    user: process.env.EMAIL_USER,
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    refreshToken: process.env.REFRESH_TOKEN,
+  },
+  connectionTimeout: 10000, // 10s connection timeout
+  greetingTimeout: 10000,
+  socketTimeout: 10000,
 });
 
 // Verify the connection configuration
